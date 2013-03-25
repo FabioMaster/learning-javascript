87c4a324ff7330f637230e5cf01fe65d405579fa
FabioMaster <fabio.mastromatteo@yahoo.it>
2013-03-25 16:09:12 +0100

    commit esercizi

---
+++ 2013-03-21/exercise02.js
@@ -0,0 +1,8 @@
+function Edge (v1,v2){
+	this.v1 = v1;
+	this.v2 = v2;
+}
+
+Edge.prototype.length = function (){
+	return Math.sqrt(Math.pow((v1.x - v2.x),2) + Math.pow((v1.y - v2.y),2));
+}
---
+++ 2013-03-21/exercise03.js
@@ -0,0 +1,20 @@
+function Triangle(v1, v2, v3){
+	this.v1 = v1;
+	this.v2 = v2;
+	this.v3 = v3;
+}
+
+Triangle.prototype.perimeter = function(){
+	lato1 = Math.sqrt(Math.pow((v1.x - v2.x),2) + Math.pow((v1.y - v2.y),2));
+	lato2 = Math.sqrt(Math.pow((v1.x - v3.x),2) + Math.pow((v1.y - v3.y),2));
+	lato3 = Math.sqrt(Math.pow((v3.x - v2.x),2) + Math.pow((v3.y - v2.y),2));
+	return (lato1 + lato2 + lato3);
+}
+
+Triangle.prototype.area = function(){
+	lato1 = Math.sqrt(Math.pow((v1.x - v2.x),2) + Math.pow((v1.y - v2.y),2));
+	lato2 = Math.sqrt(Math.pow((v1.x - v3.x),2) + Math.pow((v1.y - v3.y),2));
+	lato3 = Math.sqrt(Math.pow((v3.x - v2.x),2) + Math.pow((v3.y - v2.y),2));
+	semip = ((lato1 + lato2 + lato3) / 2);
+	return Math.sqrt(semip*(semip - lato1)*(semip - lato2)*(semip - lato3));
+}
